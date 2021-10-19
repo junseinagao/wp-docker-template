704d09c5bb23c04e9ad83bda276b971362973d82
junseinagao <junsei.nagao@gmail.com>
2021-10-19 17:30:26 +0900

    自動アップロード機能をつけた

--- original-theme/postcss.config.js
+++ original-theme/postcss.config.js
@@ -2,12 +2,6 @@ module.exports = {
   plugins: [
     require('autoprefixer')({
       grid: true,
-      /* .browserslistrcに移動
-      browsers: ['last 2 versions']
-      */
-      /*
-      browsers: ['IE 11', 'last 2 versions']
-      */
     })
   ],
 };
