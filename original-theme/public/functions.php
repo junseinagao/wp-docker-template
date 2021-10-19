dde43bc5c33d22c0571007aca1f7290a1fff80b0
junseinagao <junsei.nagao@gmail.com>
2021-10-19 18:27:44 +0900

    基本をgenericに変えた

--- original-theme/public/functions.php
+++ original-theme/public/functions.php
@@ -7,15 +7,15 @@
  * @package original-theme
  */
 
-add_action( 'wp_enqueue_scripts', 'astra_parent_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'generic_parent_theme_enqueue_styles' );
 
 /**
  * Enqueue scripts and styles.
  */
-function astra_parent_theme_enqueue_styles() {
-	wp_enqueue_style( 'astra-style', get_template_directory_uri() . '/style.css' );
+function generic_parent_theme_enqueue_styles() {
+	wp_enqueue_style( 'generic-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'original-theme-style',
 		get_stylesheet_directory_uri() . '/style.css',
-		[ 'astra-style' ]
+		[ 'generic-style' ]
 	);
 }
